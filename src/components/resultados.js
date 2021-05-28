7c0f38b64793c0927353e219ff5e5e448dd5e5f6
LuisHurtado137 <luisferhc352@gmail.com>
2021-05-28 16:19:52 -0500

    add an condition when there is no hotels found, with an hotel image and text

--- src/components/resultados.js
+++ src/components/resultados.js
@@ -1,22 +1,31 @@
 import Card from "./card/card"
+import hotelError from "./hotel-not-found/hotel.jpg"
 
 export let Resultados = (hotel) => {
+ 
   return ( 
       <div className="hotel-list">
-      {hotel.filterHotelList.map((hotel) => (
-            <Card
-              key={hotel.slug}
-              name={hotel.name}
-              photo={hotel.photo}
-              description={hotel.description}
-              rooms={hotel.rooms}
-              city={hotel.city}
-              country={hotel.country}
-              price={hotel.price}
-          />
-          ) 
-        ) 
-      } 
+        {hotel.filterHotelList.length > 0 ? (
+            hotel.filterHotelList.map((hotel) => {
+              return(
+                <Card
+                  key={hotel.slug}
+                  name={hotel.name}
+                  photo={hotel.photo}
+                  description={hotel.description}
+                  rooms={hotel.rooms}
+                  city={hotel.city}
+                  country={hotel.country}
+                  price={hotel.price}
+                />
+              );
+            })
+    ) : (
+      <div className="animation-container">
+        <h2 className="error">No se encontraron resultados</h2>
+        <img className="error-hotel" src={hotelError} alt="hotel-not-found"/>
+      </div>  
+    )}
     </div>
-  )     
+  )    
 }
