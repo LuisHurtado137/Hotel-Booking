cffaee830cd11e1011f461cc83eecc33dc71da28
LuisHurtado137 <luisferhc352@gmail.com>
2021-06-07 00:15:10 -0500

    sin cambios

---
+++ src/App/App.js
@@ -0,0 +1,142 @@
+import { hotelsData } from "../components/data/data"
+import { useState, useEffect} from "react";
+import { Filtros } from "../components/Filters/filters"
+import { Resultados }  from "../components/Resultados/resultados"
+import { Header } from "../components/header/header"
+import { dateToUnix, beforeDateStatus, dateUnixFromInput } from "../utils/dates";
+import "./App.css"
+
+function App() {
+
+  const [country, setCountry] = useState("seleccionar");
+  const [price, setPrice] = useState("seleccionar");
+  const [size, setSize] = useState("seleccionar");
+  const [userDate, setUserDate] = useState({
+    from: '',
+    to: ''
+  });
+  const [userDateUnix, setUserDateUnix] = useState({
+      from: '',
+      to: ''
+    });
+  const [filterHotelList, setfilterHotelList] = useState(hotelsData);
+
+
+  const handleDate = (key, value) => {
+    if (beforeDateStatus(value)) {
+      alert(`Seleccione una fecha igual o posterior a la fecha de hoy`)
+    } else if (key === "from" && dateUnixFromInput(value) > dateUnixFromInput(userDate.from)){
+      alert("la fecha DESDE no puede ser posterior a la fecha HASTA")
+    } else {
+      if (key === "from" ) {
+        const newUserDate = { ...userDate, [key]: value };
+        setUserDate(newUserDate);
+      } else if (
+        key === "to" &&
+        dateUnixFromInput(value) >= dateUnixFromInput(userDate.from)
+      ) 
+      {
+        const newUserDate = { ...userDate, [key]: value };
+         setUserDate(newUserDate);
+          if (newUserDate.from !== '' && newUserDate.to !== '') {
+          const newUserDateUnix = dateToUnix(newUserDate);
+          setUserDateUnix(newUserDateUnix);
+      }
+    } else {
+      alert(`la fecha HASTA debe ser mayor a la fecha DESDE`)
+    }   
+  }    
+};
+
+  const filterHotels = () => {
+    let newFilterHotels = [...hotelsData];
+    if (userDate.from !== '' && userDate.to !== '') {
+      newFilterHotels = hotelsData.filter(hotel => {
+        const hotelFrom = userDateUnix.from >= hotel.availabilityFrom;
+        const hotelTo = userDateUnix.to <= hotel.availabilityTo;
+
+        return hotelFrom && hotelTo;
+      });
+    } 
+    setfilterHotelList(newFilterHotels);
+  };
+
+  useEffect(filterHotels, [userDate, userDateUnix.from, userDateUnix.to]);
+
+  const handleCountry = (e) => {
+    const country = e.target.value;
+    setCountry(country);
+  };
+  const handlePrice = (e) => {
+    const price = e.target.value;
+    setPrice(price);
+  };
+  const handleSize = (e) => {
+    const size = e.target.value;
+    setSize(size);
+  };
+
+ 
+  useEffect(() => {
+    const countryFilter = (hotel) => {
+      return (country === "seleccionar" ? true : country === "todos" ? true : country === hotel.country )}
+    const priceFilter = (hotel) => {
+      return (price === "seleccionar" ? true : price === "todos" ? true : parseInt(price, 0) === hotel.price )}
+    const sizeFilter = (hotel) => {
+      return (
+        size === "seleccionar"
+        ? true
+        : size === "pequeño"
+        ? hotel.rooms <= 10
+        : size === "mediano"
+        ? hotel.rooms > 10 && hotel.rooms <= 20
+        : size === "grande" 
+        ? hotel.rooms > 20 
+        : size === "todos" ? true : hotel.rooms 
+      )
+    }
+
+    const filterHotelList = hotelsData.filter((hotel) => countryFilter(hotel) && priceFilter(hotel) && sizeFilter(hotel));
+    setfilterHotelList(filterHotelList);
+  }, [country, price, size]);
+  
+
+  const handleReset = (e) => {
+    const userDateEmpty = {
+      from: '',
+      to: ''
+    };
+    setUserDate(userDateEmpty);
+    setUserDateUnix(userDateEmpty);
+    setCountry("seleccionar");
+    setPrice("seleccionar");
+    setSize("seleccionar");
+  };
+
+  return (
+   <div className="app">
+      <Header
+        userDate={userDate}
+        country={country}
+        price={price}
+        size={size}
+        handleReset={handleReset}
+      />
+      <Filtros
+        userDate={userDate}
+        handleDate={handleDate}
+        country={country}
+        handleCountry={handleCountry}
+        price={price} 
+        handlePrice={handlePrice}
+        size={size}
+        handleSize={handleSize}
+        handleReset={handleReset}
+      />
+      <Resultados
+        filterHotelList={filterHotelList} 
+      />
+   </div>
+  );
+}
+export default App;
---
+++ src/components/Filters/filters.js
@@ -0,0 +1,39 @@
+export function Filtros(props) {
+    
+  return (
+      <div className="filtros">
+        <label className="from-to">
+          Desde 
+          <input type="date" value={props.userDate.from} onChange={({ target }) => props.handleDate('from', target.value)} className="date-filter" />
+        </label>
+        <label className="from-to">
+          Hasta
+          <input type="date" value={props.userDate.to} onChange={({ target }) => props.handleDate('to', target.value)} className="date-filter"/>
+        </label>
+        <select value={props.country} onChange={props.handleCountry} className="select">
+            <option disabled value="seleccionar">Seleccione el país</option>
+            <option value="todos">Cualquier País</option>
+            <option value="Argentina">Argentina</option>
+            <option value="Brasil">Brasil</option>
+            <option value="Chile">Chile</option>
+            <option value="Uruguay">Uruguay</option>
+        </select>
+        <select value={props.price} onChange={props.handlePrice} className="select">
+            <option disabled value="seleccionar">Seleccione el precio</option>
+            <option value="todos">Cualquier precio</option>
+            <option value="1">$</option>
+            <option value="2">$$</option>
+            <option value="3">$$$</option>
+            <option value="4">$$$$</option>
+        </select>
+        <select value={props.size} onChange={props.handleSize} className="select">
+            <option disabled value="seleccionar">Seleccione el tamaño</option>
+            <option value="todos">Cualquier tamaño</option>
+            <option value="pequeño">Pequeño</option>
+            <option value="mediano">Mediano</option>
+            <option value="grande">Grande</option>
+        </select>
+        <button className="reset-btn" onClick={props.handleReset}>Reset</button> 
+      </div>
+    );
+  }
